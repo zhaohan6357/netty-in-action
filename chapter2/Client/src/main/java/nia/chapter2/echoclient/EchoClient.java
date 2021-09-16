b0ff7e9c6c97f8840f05b9f0fd2730f867bf3a13
zhaohan <zhaohan@kuaishou.com>
2021-09-16 14:10:39 +0800

    minor

--- chapter2/Client/src/main/java/nia/chapter2/echoclient/EchoClient.java
+++ chapter2/Client/src/main/java/nia/chapter2/echoclient/EchoClient.java
@@ -1,5 +1,7 @@
 package nia.chapter2.echoclient;
 
+import java.net.InetSocketAddress;
+
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
@@ -8,14 +10,13 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
-import java.net.InetSocketAddress;
-
 /**
  * Listing 2.4 Main class for the client
  *
  * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
  */
 public class EchoClient {
+
     private final String host;
     private final int port;
 
@@ -24,22 +25,19 @@ public class EchoClient {
         this.port = port;
     }
 
-    public void start()
-        throws Exception {
+    public void start() throws Exception {
         EventLoopGroup group = new NioEventLoopGroup();
         try {
             Bootstrap b = new Bootstrap();
-            b.group(group)
-                .channel(NioSocketChannel.class)
-                .remoteAddress(new InetSocketAddress(host, port))
-                .handler(new ChannelInitializer<SocketChannel>() {
-                    @Override
-                    public void initChannel(SocketChannel ch)
-                        throws Exception {
-                        ch.pipeline().addLast(
-                             new EchoClientHandler());
-                    }
-                });
+            b.group(group).channel(NioSocketChannel.class)
+                    .remoteAddress(new InetSocketAddress(host, port))
+                    .handler(new ChannelInitializer<SocketChannel>() {
+
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new EchoClientHandler());
+                        }
+                    });
             ChannelFuture f = b.connect().sync();
             f.channel().closeFuture().sync();
         } finally {
@@ -47,18 +45,18 @@ public class EchoClient {
         }
     }
 
-    public static void main(String[] args)
-            throws Exception {
-        if (args.length != 2) {
+    public static void main(String[] args) throws Exception {
+        /*  if (args.length != 2) {
             System.err.println("Usage: " + EchoClient.class.getSimpleName() +
                     " <host> <port>"
             );
             return;
         }
-
+        
         final String host = args[0];
-        final int port = Integer.parseInt(args[1]);
+        final int port = Integer.parseInt(args[1]);*/
+        final String host = "127.0.0.1";
+        final int port = 12345;
         new EchoClient(host, port).start();
     }
 }
-
--- chapter2/Server/src/main/java/nia/chapter2/echoserver/EchoServer.java
+++ chapter2/Server/src/main/java/nia/chapter2/echoserver/EchoServer.java
@@ -24,13 +24,14 @@ public class EchoServer {
 
     public static void main(String[] args)
         throws Exception {
-        if (args.length != 1) {
+     /*   if (args.length != 1) {
             System.err.println("Usage: " + EchoServer.class.getSimpleName() +
                 " <port>"
             );
             return;
         }
-        int port = Integer.parseInt(args[0]);
+        int port = Integer.parseInt(args[0]);*/
+        int port = 12345;
         new EchoServer(port).start();
     }
 
